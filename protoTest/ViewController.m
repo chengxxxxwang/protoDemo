5687e9efc4bc55641ec9087b2844fcbd2354b635
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-03-29 09:37:45 +0800

    protoDemo
    protoDemo

---
+++ protoTest/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  protoTest
+//
+//  Created by chenxingwang on 16/3/28.
+//  Copyright © 2016年 chenxingwang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Person.pb.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    PBUserBuilder * person = [[[[[PBUserBuilder builder] setUserId:@"123"] setName:@"Bob"] setEmail:@"bob@example.com"] build];
+    PBUser *person1 = [[[[[PBUser builder] setUserId:@"123"]setNick:@"Bob"]setAvatar:@"Bob@Example.com"] build];
+    
+    NSData* data = [person1 data];
+ 
+    
+    NSData* raw_data = data;
+    PBUser* person = [PBUser parseFromData:raw_data];
+    
+    NSLog(@"%@",person);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
